1d5807e673b4f3a18a7485037fd6a083f9d1d410
App Academy Student <aa@Apps-Mac-mini-2.local>
2019-04-16 09:56:45 -0400

    add mancala homework

--- W2D2/Mancala/lib/board.rb
+++ W2D2/Mancala/lib/board.rb
@@ -1,20 +1,51 @@
+require_relative 'player'
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @mancala = Mancala.new(name1, name2)
+    @mancala.play
+    @cups = Array.new(13) {Array.new(4, :stone)}
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    (0..5).each do |i|
+      self.cups[i] = 4
+    end
+    (7..12).each do |j|
+      self.cups[j] = 4
+    end 
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos > 13
+    raise 'Starting cup is empty' if self.cups[start_pos].empty?
+    !self.cups[start_pos].empty? || start_pos < 14 
   end
 
   def make_move(start_pos, current_player_name)
+    until self.cups[start_pos].empty?
+      ((start_pos + 1) % 13..(start_pos + 13) % 13).each do |i|
+        if current_player_name.side == 1
+          self.cups[i] << self.cups[start_pos].shift unless i == 13
+        else
+          self.cups[i] << self.cups[start_pos].shift unless i == 6
+        end
+      end
+    end 
+    self.render
+    next_turn(i)
+
   end
 
   def next_turn(ending_cup_idx)
+    if (current_player_name.side == 1 && ending_cup_idx == 6) || (current_player_name.side == 2 && ending_cup_idx == 13)
+      :prompt
+    elsif self.cups[ending_cup_idx.empty?]
+      :switch
+    else
+      ending_cup_idx
+    end
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
   end
 
@@ -27,8 +58,21 @@ class Board
   end
 
   def one_side_empty?
+
+    (0..5).each do |i|
+      return false if !self.cups[i].empty?
+    end
+    (7..12).each do |j|
+      return false if !self.cups[j].empty?
+    end
+
+    true
   end
 
   def winner
+    if one_side_empty? && self.cups[6] == self.cups[13] 
+        return :draw
+    elsif one_side_empty? && self.cups[6] > self.cups[13]
+        return current_player_name.side == 1
   end
 end
